977a52ac872cf266abcbe6e2b59e7ae239421ecc
ayoub Deguiri <vodkaayoub@gmail.com>
2025-01-22 15:47:08 +0100

    SS

--- callback.php
+++ callback.php
@@ -4,8 +4,8 @@ require 'vendor/autoload.php';
 use League\OAuth2\Client\Provider\GenericProvider;
 
 $provider = new GenericProvider([
-    'clientId'                => 'aw8npq7v82k8d0dr', // Replace with your TikTok Client Key
-    'clientSecret'            => 'fJasINgka1VI8QKzOWDAPUuTEFDBbeEm', // Replace with your TikTok Client Secret
+    'clientId'                => 'sbaww4cmy7s57rnq3j', // Replace with your TikTok Client Key
+    'clientSecret'            => 'W0ag11JJVIEglBoMKgplUyApTt7J1wHH', // Replace with your TikTok Client Secret
     'redirectUri'             => 'https://encouraging-crissie-dakiri-28cc87b6.koyeb.app/callback.php', // Replace with your Koyeb app URL
     'urlAuthorize'            => 'https://www.tiktok.com/v2/auth/authorize/',
     'urlAccessToken'          => 'https://www.tiktok.com/v2/auth/token/',
