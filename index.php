b229d9c61b64913d488eb96ae97570acc10649ed
ayoub Deguiri <vodkaayoub@gmail.com>
2025-01-22 11:15:52 +0100

    dd

--- index.php
+++ index.php
@@ -1,18 +1,2 @@
-<?php
-require 'vendor/autoload.php';
-
-use League\OAuth2\Client\Provider\GenericProvider;
-
-$provider = new GenericProvider([
-    'clientId'                => 'aw8npq7v82k8d0dr', // Replace with your TikTok Client Key
-    'clientSecret'            => 'fJasINgka1VI8QKzOWDAPUuTEFDBbeEm', // Replace with your TikTok Client Secret
-    'redirectUri'             => 'https://encouraging-crissie-dakiri-28cc87b6.koyeb.app/callback.php', // Replace with your Koyeb app URL
-    'urlAuthorize'            => 'https://www.tiktok.com/v2/auth/authorize/',
-    'urlAccessToken'          => 'https://www.tiktok.com/v2/auth/token/',
-    'urlResourceOwnerDetails' => 'https://www.tiktok.com/v2/resource/owner/',
-]);
-
-// Redirect to TikTok's OAuth login page
-$authUrl = $provider->getAuthorizationUrl();
-header('Location: ' . $authUrl);
-exit;
+<?php 
+echo "helooo world";
